8655040daf5d88cef667eef2082944496bdbc927
Daffaislam16 <fatahdaffa1616@gmail.com>
2025-02-20 01:55:28 +0700

    Create Adhika.py

---
+++ Tim/Adhika.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM:", nim)
+    print("Peran saya yaitu:", peran)
+
+if __name__ == "__main__":
+    nama = "Adhika Tyo Ferdiansyah"
+    nim = "22.130.10.366"
+    peran = "Member"
+    perkenalan(nama, nim, peran)
